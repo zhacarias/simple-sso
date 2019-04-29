781fd4fb8a227cb6f929d6c6b3aba85f12ed1829
Mark Kirshner <markkirshner.m.chico@smretail.com>
2019-04-29 16:57:10 +0800

    Remove unused route

--- src/routes.php
+++ src/routes.php
@@ -5,14 +5,6 @@ use Slim\Http\Response;
 
 // Routes
 
-//$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-//    // Sample log message
-//    $this->logger->info("Slim-Skeleton '/' route");
-//
-//    // Render index view
-//    return $this->renderer->render($response, 'index.phtml', $args);
-//});
-
 $app->get('/', function () use ($app) {
     return 'It Works';
 });
