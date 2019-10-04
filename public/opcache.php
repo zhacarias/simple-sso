b1a4fe6f047257e7ac449caff7c52be7aee4a249
Mark Kirshner Chico <markkirshner.m.chico@smretail.com>
2019-10-04 21:18:00 +0800

    opcache remove data-sample

--- public/opcache.php
+++ public/opcache.php
@@ -3,8 +3,8 @@
 define('THOUSAND_SEPARATOR',true);
 
 if (!extension_loaded('Zend OPcache')) {
-    echo '<div style="background-color: #F2DEDE; color: #B94A48; padding: 1em;">You do not have the Zend OPcache extension loaded, sample data is being shown instead.</div>';
-    require 'data-sample.php';
+    echo '<div style="background-color: #F2DEDE; color: #B94A48; padding: 1em;">You do not have the Zend OPcache extension loaded.</div>';
+    die;
 }
 
 class OpCacheDataModel
